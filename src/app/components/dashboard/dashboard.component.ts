691f5eafe8eb00d8db7c38b8c72bec252cd87120
[Rvjonh] <[jonhvelasco3@gmail.com]>
2023-07-06 12:11:51 -0400

    app 1v

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WeatherService } from 'src/app/services/weather.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  city:string = '';
+  query:boolean = false;
+  loading:boolean = false;
+  errorMessage:string = '';
 
+  cityName:string = '';
+  countrySys:string = '';
+  description:string = '';
+  temperature:number = 0;
+  humidity:number = 0;
+  typeWeather:string = '';
+
+  constructor(private _weatherService: WeatherService){}
+
+  getWeather(){
+    this.loading = true;
+
+    this._weatherService.getWeather(this.city).subscribe((data)=>{
+      this.query = true;
+      this.loading = false;
+      this.city = '';
+      this.cityName = data.name;
+      this.countrySys = data.sys.country;
+      this.description = data.weather[0].description;
+      this.humidity = data.main.humidity;
+      this.temperature = Math.trunc(data.main.temp - 273);
+      this.typeWeather = data.weather[0].main;
+
+      this.errorMessage = '';
+    }, error =>{
+      this.query = false;
+      this.loading = false;
+      this.city = '';
+      this.errorMessage = error.error.message;
+      this.hideErrorMessage();
+    })
+  }
+
+  hideErrorMessage(){
+    /* hide component in template, async with css animation */
+    setTimeout(() => {
+      this.errorMessage = '';
+    }, 2500);
+  }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  //URL = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}';
+  /* do not steal my api key, angular sucks and too complicated config .env files */
+  endPoint = "79fee46b93f7e4703820a353be0cd666";
+  
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getWeather(city: string): Observable<any>{
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.endPoint}`;
+    return this.http.get(URL);
+  }
 }
